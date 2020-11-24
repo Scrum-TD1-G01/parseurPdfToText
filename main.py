8787ab11c475b41a972f2b94d6d594fa6dcf523c
Raphael Rey <raphael.rey@alumni.univ-avignon.fr>
2020-11-24 12:41:26 +0100

    Ajout de fonctionalité ouvrir le fichier pdf passé en argument 1,sortie et ecrire le nom du fichier original dans la sortie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pdftotext
+import sys
 
 # Load your PDF
 with open("./testRessources/Boudin-Torres-2006.pdf", "rb") as f:
@@ -7,6 +8,10 @@ with open("./testRessources/Boudin-Torres-2006.pdf", "rb") as f:
 # How many pages?
 print(len(pdf))
 
+#recuperer le pdf source
+pdfsource = open(sys.argv[1],"r")
+sortie = open(sys.argv[1]+"_parser.txt","w")#delete le dernier fichier si il existe
+sortie.write("Fichier Original: \n"+"    "+sys.argv[1])
 # Iterate over all the pages
 for page in pdf:
     print(page)
