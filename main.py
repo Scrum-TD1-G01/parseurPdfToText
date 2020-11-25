b28c7f4ecfc61ee4b37a228149847654ae0af666
Raphael Rey <raphael.rey@alumni.univ-avignon.fr>
2020-11-25 19:24:45 +0100

    Ajout de la fonctionalité recuperer l'abstract a partir d'un pdf

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import pdftotext
 import sys
+import unicodedata
+import os
 
-# Load your PDF
-with open("./testRessources/Boudin-Torres-2006.pdf", "rb") as f:
-    pdf = pdftotext.PDF(f)
-
-# How many pages?
-print(len(pdf))
+# Convert pdf
+os.system("pdftotext -nopgbrk "+sys.argv[1])
+pdf = open(os.path.splitext(sys.argv[1])[0]+'.txt',"r")
 
 #recuperer le pdf source
-pdfsource = open(sys.argv[1],"r")
 sortie = open(sys.argv[1]+"_parser.txt","w")#delete le dernier fichier si il existe
-sortie.write("Fichier Original: \n"+"    "+sys.argv[1])
-# Iterate over all the pages
-for page in pdf:
-    print(page)
+sortie.write("Fichier Original: \n"+"    "+sys.argv[1]+"\n")
+sortie.write("Abstract: \n")
+#recuperer l'abstract
+lines = pdf.readlines()
+copy = False
+for line in lines:
+	if str("Abstract") in line:
+		copy = True
+	elif str("ABSTRACT") in line:
+		copy = True
+	elif str("1\n") in line:
+		copy = False
+	elif str("I.") in line:
+		copy = False
+	elif str("1.\n") in line:
+		copy = False
+	elif copy:
+		sortie.write(line.strip())
+		
+		
+		
+		
+sortie.close()
+pdf.close()
 
-# Read some individual pages
-print(pdf[0])
-print(pdf[1])
 
-# Read all the text into one string
-print("\n\n".join(pdf))
