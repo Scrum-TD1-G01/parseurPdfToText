da702583ae247f374f6bd35759ccfa7218c611cf
Nathanael-Lefevre <nathanael.lefevre@alumni.univ-avignon.fr>
2020-11-25 23:50:20 +0100

    add : recupération du titre (lazy mode)

--- main.py
+++ main.py
@@ -3,16 +3,27 @@ import sys
 import unicodedata
 import os
 
+
 # Convert pdf
 os.system("pdftotext -nopgbrk "+sys.argv[1])
 pdf = open(os.path.splitext(sys.argv[1])[0]+'.txt',"r")
 
-#recuperer le pdf source
-sortie = open(sys.argv[1]+"_parser.txt","w")#delete le dernier fichier si il existe
+
+# recuperer le pdf source
+sortie = open(sys.argv[1]+"_parser.txt","w")  # delete le dernier fichier si il existe
 sortie.write("Fichier Original: \n"+"    "+sys.argv[1]+"\n")
-sortie.write("Abstract: \n")
-#recuperer l'abstract
+
+
 lines = pdf.readlines()
+# recuperer le titre
+
+sortie.write("Title: \n")
+sortie.write(lines[0].strip()+'\n')
+
+
+# recuperer l'abstract
+sortie.write("Abstract: \n")
+
 copy = False
 for line in lines:
 	if str("Abstract") in line:
@@ -27,10 +38,8 @@ for line in lines:
 		copy = False
 	elif copy:
 		sortie.write(line.strip())
-		
-		
-		
-		
+
+sortie.write('\n')
 sortie.close()
 pdf.close()
 
